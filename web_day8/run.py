d6d7c5c1bbda7edc1b86ef47313e9e33a9d52912
danbaku <1366796837@qq.com>
2023-07-29 08:59:35 +0800

    东软实训笔记及作业

---
+++ web_day8/run.py
@@ -0,0 +1,15 @@
+"""
+作者：张浩
+时间：2023/7/27  9:27
+邮箱：88302599@qq.com
+"""
+
+import pytest
+
+# pytest.main(['--report=report.html',
+#              '--title=测试报告标题',
+#              '--tester=张浩',
+#              '--desc=报告描述信息',
+#              '--template=1'])
+
+pytest.main(['--alluredir=allure_report'])
---
+++ web_day8/testcase/test_demo02.py
@@ -0,0 +1,19 @@
+"""
+作者：张浩
+时间：2023/7/27  9:24
+邮箱：88302599@qq.com
+"""
+
+class TestClass:
+
+    def testclass_001(self):
+        print("-----------testclass_001--------------")
+        assert 1 == 1
+
+    def testclass_002(self):
+        print("-----------testclass_002--------------")
+        assert 1 == 100
+
+    def testclass_003(self):
+        print("-----------testclass_003--------------")
+        assert 1 == 1
